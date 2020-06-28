1fc82912d190c7cbf51677f6ff62ea82cc9b6b9d
Javier Gómez Luzón <javo14795@gmail.com>
2020-06-28 14:28:51 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DungeonDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
