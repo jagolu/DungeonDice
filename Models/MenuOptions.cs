d947491c8f5af2cb3be1b513e73c7f436a6e9913
Javier Gómez Luzón <javo14795@gmail.com>
2020-06-28 16:45:45 +0200

    Create InitialMenu

--- Models/MenuOptions.cs
+++ Models/MenuOptions.cs
@@ -3,5 +3,9 @@
     enum MenuOptions
     {
         FAIL = 0,
+        NEWGAME = 1,
+        LOADGAME = 2,
+        STATICS = 3,
+        QUIT = 4
     }
 }
---
+++ Src/InitialMenu.cs
@@ -0,0 +1,40 @@
+﻿using DungeonDice.Models;
+using System;
+using System.Collections.Generic;
+
+namespace DungeonDice.Src
+{
+    class InitialMenu : Menu
+    {
+        public InitialMenu() : base ()
+        {
+            this.options = new List<int>() { 1, 2, 3, 4 };
+        }
+
+        public override void showOptions()
+        {
+            Console.WriteLine("1) New Game");
+            Console.WriteLine("2) Load Game");
+            Console.WriteLine("3) Statics");
+            Console.WriteLine("4) Quit");
+            Console.WriteLine("\n");
+        }
+
+        public override MenuOptions parseOption(int selectedOption)
+        {
+            switch (selectedOption)
+            {
+                case 1:
+                    return MenuOptions.NEWGAME;
+                case 2:
+                    return MenuOptions.LOADGAME;
+                case 3:
+                    return MenuOptions.STATICS;
+                case 4:
+                    return MenuOptions.QUIT;
+            }
+
+            return MenuOptions.FAIL;
+        }
+    }
+}
