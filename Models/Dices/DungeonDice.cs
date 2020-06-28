da975014de8fa5f35b786a7aa89f6926762e633e
Javier Gómez Luzón <javo14795@gmail.com>
2020-06-28 15:47:00 +0200

    Refactor DungeonDice

---
+++ Models/Dices/DungeonDice.cs
@@ -0,0 +1,39 @@
+﻿using DungeonDice.Models.Characters;
+
+namespace DungeonDice.Models.Dices
+{
+    class DungeonDice : Dice
+    {
+        public DungeonDice (): base()
+        {
+            this.category = Category.BAD;
+        }
+
+        public override void roll()
+        {
+            this.rollDice();
+
+            switch (this.actualValue)
+            {
+                case 1:
+                    this.character = new Zombie();
+                    break;
+                case 2:
+                    this.character = new Potion();
+                    break;
+                case 3:
+                    this.character = new Chest();
+                    break;
+                case 4:
+                    this.character = new Skeleton();
+                    break;
+                case 5:
+                    this.character = new Dragon();
+                    break;
+                case 6:
+                    this.character = new SwampCreature();
+                    break;
+            }
+        }
+    }
+}
