15c9c034bdf1729ce71a514545a3763fde3cbc35
Javier Gómez Luzón <javo14795@gmail.com>
2020-06-28 18:40:34 +0200

    Add PrizesChest

---
+++ Src/Prizes/PrizesChest.cs
@@ -0,0 +1,55 @@
+﻿using DungeonDice.Models.Prizes;
+using DungeonDice.Src.Prizes;
+using DungeonDice.Src.Util;
+using System;
+using System.Collections.Generic;
+
+namespace DungeonDice.Src
+{
+    class PrizesChest
+    {
+        private List<Prize> prizes;
+
+        public PrizesChest()
+        {
+            PrizeInitializer prizeInitializer = new PrizeInitializer();
+            this.prizes = prizeInitializer.Initialize();
+            this.randomize();
+        }
+
+        public Prize getPrize()
+        {
+            Prize prize = this.prizes[0];
+            this.prizes.RemoveAt(0);
+
+            return prize;
+        }
+
+        public void returnPrizeToChest(Prize prize)
+        {
+            if (CheckPrizeType.isDragonEgg(prize))
+            {
+                Console.WriteLine("No puedes usar este objeto");
+                return;
+            }
+
+            this.prizes.Add(prize);
+            this.randomize();
+        }
+
+        private void randomize()
+        {
+            Random rnd = new Random();
+            int n = this.prizes.Count;
+
+            while(n > 1)
+            {
+                n--;
+                int k = rnd.Next(n + 1);
+                Prize value = this.prizes[k];
+                this.prizes[k] = this.prizes[n];
+                this.prizes[n] = value;
+            }
+        }
+    }
+}
