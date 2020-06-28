80df5e1ee79a1666ab1ab0fbfd5d45b2076f977b
Javier Gómez Luzón <javo14795@gmail.com>
2020-06-28 15:46:52 +0200

    Refactor Dice

---
+++ Models/Dices/Dice.cs
@@ -0,0 +1,28 @@
+﻿using DungeonDice.Models.Characters;
+using System;
+
+namespace DungeonDice.Models.Dices
+{
+    abstract class Dice
+    {
+        public int actualValue;
+        public Category category;
+        public Character character;
+
+        public Dice() {
+            this.actualValue = 0;
+        }
+
+        public void rollDice()
+        {
+            var rand = new Random();
+            this.actualValue = rand.Next(1, 7);
+        }
+
+        public abstract void roll();
+        public Character getValue()
+        {
+            return this.character;
+        }
+    }
+}
