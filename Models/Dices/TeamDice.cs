0e09995588157f21ebe72b092a8e60c126494cd0
Javier Gómez Luzón <javo14795@gmail.com>
2020-06-28 15:47:05 +0200

    Create TeamDice

---
+++ Models/Dices/TeamDice.cs
@@ -0,0 +1,39 @@
+﻿using DungeonDice.Models.Characters;
+
+namespace DungeonDice.Models.Dices
+{
+    class TeamDice : Dice
+    {
+        public TeamDice() : base()
+        {
+            this.category = Category.GOOD;
+        }
+
+        public override void roll()
+        {
+            this.rollDice();
+
+            switch (this.actualValue)
+            {
+                case 1:
+                    this.character = new Champion();
+                    break;
+                case 2:
+                    this.character = new Cleric();
+                    break;
+                case 3:
+                    this.character = new Fighter();
+                    break;
+                case 4:
+                    this.character = new QuestScroll();
+                    break;
+                case 5:
+                    this.character = new Rogue();
+                    break;
+                case 6:
+                    this.character = new Wizard();
+                    break;
+            }
+        }
+    }
+}
