358b2e7e0f4e7e2b74226707f89aed8a95fcc918
Javier Gómez Luzón <javo14795@gmail.com>
2020-06-29 19:34:30 +0200

    Create singleton game

---
+++ Src/DungeonDiceGame.cs
@@ -0,0 +1,18 @@
+namespace DungeonDice.Src.Prizes
+{
+    public class DungeonDiceGame
+    {
+    #region singleton
+        private DungeonDiceGame() { }
+        private static DungeonDiceGame _instance;
+
+        public static DungeonDiceGame getInstance()
+        {
+            if(_instance == null)
+            {
+                _instance = new DungeonDiceGame();
+            }
+            return _instance;
+        }
+    #endregion
+}
