f4fa161b160a43984f218248c1cacd73f7147cde
Javier Gómez Luzón <javo14795@gmail.com>
2020-07-01 18:20:39 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,11 +2,19 @@
 
 namespace DungeonDice
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("Hello World!");
+
+            /**
+             * Ejemplo de instanciar singleton
+             * TeamDices td = TeamDices.getInstance();
+             */
+
         }
     }
 }
