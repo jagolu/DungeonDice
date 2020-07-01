54bff92aa170fb096e09c67bfd9ebccc25cec2c4
Javier Gómez Luzón <javo14795@gmail.com>
2020-07-01 22:48:12 +0200

    Refactor specficheroselection

--- Src/Menus/ConfirmSpecificHeroSelectionMenu.cs
+++ Src/Menus/ConfirmSpecificHeroSelectionMenu.cs
@@ -6,12 +6,12 @@ namespace DungeonDice.Src.Menus
 {
     class ConfirmSpecificHeroSelectionMenu : Menu
     {
-        private readonly BasicHero heroToShow;
+        private BasicHero heroToShow;
 
-        public ConfirmSpecificHeroSelectionMenu(BasicHero heroToShow) : base()
+        public ConfirmSpecificHeroSelectionMenu(MenuOptions beforeMenuOption) : base()
         {
-            this.heroToShow = heroToShow;
             this.maxOption = 2;
+            this.getHero(beforeMenuOption);
         }
 
         public override void showOptions()
@@ -38,6 +38,37 @@ namespace DungeonDice.Src.Menus
             }
 
             return chosenOption;
+        }
+
+        private void getHero(MenuOptions option)
+        {
+            switch (option)
+            {
+                case MenuOptions.SELECT_CRUSADER:
+                    this.heroToShow = new Crusader();
+                    break;
+                case MenuOptions.SELECT_ENCHANTRESS:
+                    this.heroToShow = new Enchantress();
+                    break;
+                case MenuOptions.SELECT_HALFGOBLIN:
+                    this.heroToShow = new HalfGoblin();
+                    break;
+                case MenuOptions.SELECT_KNIGHT:
+                    this.heroToShow = new Knight();
+                    break;
+                case MenuOptions.SELECT_MERCENARY:
+                    this.heroToShow = new Mercenary();
+                    break;
+                case MenuOptions.SELECT_MINSTREL:
+                    this.heroToShow = new Minstrel();
+                    break;
+                case MenuOptions.SELECT_OCCULTIST:
+                    this.heroToShow = new Occultist();
+                    break;
+                case MenuOptions.SELECT_SPELLSWORD:
+                    this.heroToShow = new Spellsword();
+                    break;
+            }
         }
     }
 }
