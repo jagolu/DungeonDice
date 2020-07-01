4e829cd5712af2b4b299f1cb7c244822ea6ee93f
Javier Gómez Luzón <javo14795@gmail.com>
2020-07-01 19:47:24 +0200

    Update Hero.cs

--- Models/Heroes/Hero.cs
+++ Models/Heroes/Hero.cs
@@ -9,5 +9,10 @@
         public abstract void executeHability();
 
         public abstract void executeSpeciality();
+
+        public string getHeroName()
+        {
+            return this.name;
+        }
     }
 }
