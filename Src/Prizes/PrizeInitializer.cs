1f445492a08d50c23e6b5b5cc8674509d8e80969
Javier Gómez Luzón <javo14795@gmail.com>
2020-07-01 18:08:19 +0200

    change "for" for a function

--- Src/Prizes/PrizeInitializer.cs
+++ Src/Prizes/PrizeInitializer.cs
@@ -1,6 +1,7 @@
 ﻿using DungeonDice.Models.Prizes;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DungeonDice.Src.Prizes
 {
@@ -22,7 +23,7 @@ namespace DungeonDice.Src.Prizes
 
         private void addPrizes(int number, Prize prize, List<Prize> prizes)
         {
-            for (int i = 0; i < number; i++) prizes.Add(prize);
+            prizes.AddRange(Enumerable.Repeat(prize, number).ToList());
         }
 
         private List<Tuple<Prize, int>> getAvailablePrizes()
