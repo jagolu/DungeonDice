17d602f0562c59c0019965b7e8afc3f54113c844
Javier Gómez Luzón <javo14795@gmail.com>
2020-07-01 20:06:46 +0200

    Add confirm specific hero selection menu

---
+++ Src/Menus/ConfirmSpecificHeroSelectionMenu.cs
@@ -0,0 +1,43 @@
+﻿using DungeonDice.Models;
+using DungeonDice.Models.Heroes;
+using System;
+
+namespace DungeonDice.Src.Menus
+{
+    class ConfirmSpecificHeroSelectionMenu : Menu
+    {
+        private readonly BasicHero heroToShow;
+
+        public ConfirmSpecificHeroSelectionMenu(BasicHero heroToShow) : base()
+        {
+            this.heroToShow = heroToShow;
+            this.maxOption = 2;
+        }
+
+        public override void showOptions()
+        {
+            this.heroToShow.showFullHeroInformation();
+            Console.WriteLine("\n1) Confirm selection");
+            Console.WriteLine("2) Back");
+        }
+
+        public override MenuOptions parseOption(int selectedOption)
+        {
+            MenuOptions chosenOption;
+            switch (selectedOption)
+            {
+                case 1:
+                    chosenOption = MenuOptions.CONFIRM_SPECIFIC_HERO_SELECTION;
+                    break;
+                case 2:
+                    chosenOption = MenuOptions.CONFIRM_SPECIFIC_HERO_SELECTION_BACK;
+                    break;
+                default:
+                    chosenOption = MenuOptions.FAIL;
+                    break;
+            }
+
+            return chosenOption;
+        }
+    }
+}
