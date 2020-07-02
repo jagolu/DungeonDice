6cae08d5e82e0fda53467e6ff3d9b1b124c4ed6b
Javier Gómez Luzón <javo14795@gmail.com>
2020-07-02 19:05:52 +0200

    Create DefaultBasicHero.cs

---
+++ Models/Heroes/BasicHeroes/DefaultBasicHero.cs
@@ -0,0 +1,26 @@
+﻿using DungeonDice.Models.Heroes.EvolvedHeroes;
+using System;
+
+namespace DungeonDice.Models.Heroes.BasicHeroes
+{
+    class DefaultBasicHero : BasicHero
+    {
+        public DefaultBasicHero() : base()
+        {
+            this.name = "Default basic hero";
+            this.speciality = "This hero has no speciality";
+            this.hability = "This hero has no hability";
+            this.evolvedHero = new DefaultEvolvedHero();
+        }
+
+        public override void executeHability()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void executeSpeciality()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
